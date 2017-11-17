b0a4e2e8f9ea9e3f5f5e09744a4906daa02b59eb
Stephen Chapman <schapm16@gmail.com>
2017-11-17 02:11:04 +0000

    MVP 100%, Known breaks: 1) Invalid user input format 2) Duplicate train names will break periodic update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var database = firebase.database();
 var dbRef = database.ref();
 
 // Global function to perform time math and print data to screen.
-  function postData(trainName, trainDestination, firstTrainTime, trainFrequency, currentTime) {
+  function postData(trainName, trainDestination, firstTrainTime, trainFrequency, currentTime, periodic = false) {
     var timeUntilNext;
     var nextArrival;
     
@@ -22,21 +22,27 @@ var dbRef = database.ref();
       nextArrival = currentTime.add(timeUntilNext,"minutes");
     }
     
-    $(".table").append($("<tr>"));
+    if (periodic === false) {
+      $(".table").append($("<tr>").addClass(trainName));
+      
+      $("tr:last").append("<td>" + trainName + "</td>");
+      $("tr:last").append("<td>" + trainDestination + "</td>");
+      $("tr:last").append("<td>" + trainFrequency + "</td>");
+      $("tr:last").append("<td>" + nextArrival.format("HH:mm") + "</td>");
+      $("tr:last").append("<td>" + timeUntilNext + "</td>");
+    }
     
-    
-    $("tr:last").append("<td>" + trainName + "</td>");
-    $("tr:last").append("<td>" + trainDestination + "</td>");
-    $("tr:last").append("<td>" + trainFrequency + "</td>");
-    $("tr:last").append("<td>" + nextArrival.format("HH:mm") + "</td>");
-    $("tr:last").append("<td>" + timeUntilNext + "</td>");
+    if (periodic === true) {
+      $("." + trainName + " td").eq(3).text(nextArrival.format("HH:mm"));
+      $("." + trainName + " td").eq(4).text(timeUntilNext);
+    }
   }
 
 
 
 $(function() {
   
-  //Function to pull data from database and update current time stamp on page load.
+  //Function to pull data from database and update current time stamp on initial page load.
   dbRef.once('value', function(snapshot) {
     snapshot.forEach(function(childSnapshot) {
       
@@ -68,7 +74,7 @@ $(function() {
       trainFrequency: trainFrequency,
       timeStamp: firebase.database.ServerValue.TIMESTAMP
     });
-    
+        
     dbRef.once("child_added", function(snapshot) {
       currentTime = moment(snapshot.val().timeStamp);
     });
@@ -76,6 +82,30 @@ $(function() {
     postData (trainName, trainDestination, firstTrainTime, trainFrequency, currentTime);
     
   });
+  
+  function periodicUpdate() {
+    dbRef.once('value', function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        
+        var periodic = true;
+        
+        dbRef.child(childSnapshot.key).update({timeStamp: firebase.database.ServerValue.TIMESTAMP});
+        
+        var trainName = childSnapshot.val().trainName;
+        var trainDestination = childSnapshot.val().trainDestination;
+        var firstTrainTime = childSnapshot.val().firstTrainTime;
+        var trainFrequency = childSnapshot.val().trainFrequency;
+        var currentTime = moment(childSnapshot.val().timeStamp);
+        
+        postData (trainName, trainDestination, firstTrainTime, trainFrequency, currentTime, periodic);
+        
+      });
+    });
+  }
+  
+  //Set interval for periodic data update
+  setInterval(periodicUpdate, 30000);
+  
 });
 
 
